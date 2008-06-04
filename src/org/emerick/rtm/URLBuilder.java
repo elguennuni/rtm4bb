8e3ed6f53e770899b0f5f9bab9f826514c94923a
jaschroe <jaschroe@0363094a-1943-0410-835e-8fda14f7c62d>
2008-06-04 07:21:54 +0000

    Several optimizations and added Javadoc.

--- src/org/emerick/rtm/URLBuilder.java
+++ src/org/emerick/rtm/URLBuilder.java
@@ -6,51 +6,60 @@
 
 package org.emerick.rtm;
 
-import net.rim.device.api.crypto.MD5Digest;
-import java.util.Vector;
 import java.io.IOException;
+import java.util.Vector;
+import java.lang.StringBuffer;
+
+import net.rim.device.api.crypto.MD5Digest;
 import net.rim.device.api.util.Arrays;
 
 /**
+ * Construct a REST API HTTP GET URL request to RememberTheMilk
  * 
  * @author Jason Emerick
+ * @see #append(String, String)
+ * @see #getURL()
  */
 public class URLBuilder {
     
-    private String url;
+    private StringBuffer url;
     private String secret;
     private Vector values;
     private int count;
-    static final private String authURL = "http://www.rememberthemilk.com/services/auth/?";
-    static final private String requestURL = "http://api.rememberthemilk.com/services/rest/?";
+    private static final String authURL = "http://www.rememberthemilk.com/services/auth/?";
+    private static final String requestURL = "http://api.rememberthemilk.com/services/rest/?";
     private static final char[] hexChars ={'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'}; 
     
     public URLBuilder(String secret, boolean auth)
     {
         this.secret = secret;
+        this.url = new StringBuffer();
         if(auth)
         {
-            url = authURL;
+            url.append(authURL);
         }
         else
         {
-            url = requestURL;
+            url.append(requestURL);
         }
         values = new Vector();
         count = 0;
     }
     
+    /**
+     * Append a key-value HTTP parameter to this URL request
+     * @param name key name
+     * @param value key's value
+     */
     public void append(String name, String value)
     {
         if(count > 0)
         {
-            url += "&";
+            url.append('&');
         }
         count++;
         
-        url += name;
-        url += "=";
-        url += value;
+        url.append(name).append('=').append(value);
         
         String signature = name + value;
         
@@ -63,74 +72,83 @@ public class URLBuilder {
         values.copyInto(sigs);
         Arrays.sort(sigs, new StringComparator());
         
-        String signature = secret;
-        for(int x = 0; x < sigs.length; x++)
+        StringBuffer signature = new StringBuffer();
+        signature.append(secret);
+        int len = sigs.length;
+        for(int x = 0; x < len; x++)
         {
-            signature += sigs[x];
+            signature.append(sigs[x]);
         }
                 
-        signature = md5(signature);
-        url += ("&api_sig=" + signature);
+        url.append("&api_sig=").append(md5(signature.toString()));
         
-        url = URLEncode(url);
-        
-        return url;
+        return URLEncode(url.toString());
     }     
     
-    private String URLEncode(String url)
+    /**
+     * URL-Escape a URL
+     * @param url input url
+     * @return URL-encoded URL
+     */
+    private static String URLEncode(String url)
     {
-        String rv = "";
+    	//TODO consider URLEncodedPostData.class
+        StringBuffer rv = new StringBuffer();
         int beginindex = 0;
         int endindex = url.indexOf(' ');
         
         while (endindex != -1)
         {
-            rv += url.substring(beginindex, endindex);
-            rv += "%20";
+            rv.append(url.substring(beginindex, endindex));
+            rv.append("%20");
             beginindex = endindex + 1;
             endindex = url.indexOf(' ', beginindex);
         }
         
-        rv += url.substring(beginindex);
+        rv.append(url.substring(beginindex));
         
-        return rv;       
+        return rv.toString();       
     }
     
-    private String hexStringFromBytes(byte[] b)
+    private static String hexStringFromBytes(byte[] b)
     {
-    
-        String hex = "";
-        
+        StringBuffer hex = new StringBuffer();
         int msb;
-        
-        int lsb = 0;
+        int lsb;
         int i;
         
         // MSB maps to idx 0
         
         for (i = 0; i < b.length; i++)
         {
+        	//TODO consider Integer.toHexString(int)
             msb = ((int)b[i] & 0x000000FF) / 16;
-        
             lsb = ((int)b[i] & 0x000000FF) % 16;
-            hex = hex + hexChars[msb] + hexChars[lsb];
+            hex.append(hexChars[msb]);
+            hex.append(hexChars[lsb]);
         }
-        return(hex);
+        return(hex.toString());
     } 
     
-    private String md5(String str) throws RTMException
+    /**
+     * Get the MD5 hash of a given UTF-8 String, and return it as hex-encoded
+     * @param str String to hash
+     * @return a hex-encoded MD5 hash of the input string
+     * @throws RTMException any exception from getting the digest
+     */
+    private static String md5(String str) throws RTMException
     {
         try
         {
             MD5Digest md5 = new MD5Digest();
-        
-            md5.update(str.getBytes("UTF-8"), 0,str.getBytes("UTF-8").length );
+            byte[] inputBytes = str.getBytes("UTF-8");
+            md5.update(inputBytes, 0, inputBytes.length );
             
-            byte[] buffer = new byte[md5.getDigestLength()];
+            byte[] outputBytes = new byte[md5.getDigestLength()];
                    
-            md5.getDigest(buffer, 0, true);
+            md5.getDigest(outputBytes, 0, true);
             
-            String hash = hexStringFromBytes(buffer);
+            String hash = hexStringFromBytes(outputBytes);
             
             return hash;
         }
